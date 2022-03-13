baf18c87922674b2ea12055b0e67ab7d96924fc9
katina-anachkova <katianachkova@gmail.com>
2022-03-13 11:26:54 +0200

    extract navigation

--- movies-app/src/app/app.module.ts
+++ movies-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MovieComponent } from './movie/movie.component';
+import { MoviesComponent } from './movies/movies.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieComponent,
+    MoviesComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule
